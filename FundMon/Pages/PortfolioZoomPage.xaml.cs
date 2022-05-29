bbb1c8a518a2797c919b3cee53cbdae123292e7c
Iledant <christophe.saintillan@gmail.com>
2022-05-29 17:01:27 +0200

    Refactor UI for portfolio zoom

--- FundMon/Pages/PortfolioZoomPage.xaml.cs
+++ FundMon/Pages/PortfolioZoomPage.xaml.cs
@@ -1,7 +1,8 @@
-﻿using FundMon.Helpers;
-using FundMon.Repository;
+﻿using FundMon.Repository;
 using FundMon.ViewModel;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Globalization;
@@ -13,8 +14,7 @@ public sealed partial class PortfolioZoomPage : Page
 {
     private Portfolio SelectedPortfolio = null;
     private PortfolioZoomViewModel ViewModel = null;
-    private FundPerformance selectedFundPerformance = null;
-    static CultureInfo ci = new("fr-FR");
+    static readonly CultureInfo ci = new("fr-FR");
     private double averageCost;
     public PortfolioZoomPage()
     {
@@ -37,14 +37,16 @@ public sealed partial class PortfolioZoomPage : Page
         base.OnNavigatedTo(e);
     }
 
-    private async void FundSearchButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private async void FundSearchButton_Click(object sender, RoutedEventArgs e)
     {
-        FundSearchProgressRing.Visibility = Microsoft.UI.Xaml.Visibility.Visible;
+        FundSearchProgressRing.Visibility = Visibility.Visible;
         FundSearchButton.IsEnabled = false;
         await Task.Delay(1);
+        FundSearchGridView.Visibility = Visibility.Visible;
+        StepTwoGrid.Visibility = Visibility.Visible;
         int _ = await ViewModel.FetchMorningstarResults(FundSearchTextBox.Text);
         await Task.Delay(1);
-        FundSearchProgressRing.Visibility = Microsoft.UI.Xaml.Visibility.Collapsed;
+        FundSearchProgressRing.Visibility = Visibility.Collapsed;
         FundSearchButton.IsEnabled = true;
     }
 
@@ -53,21 +55,26 @@ public sealed partial class PortfolioZoomPage : Page
         FundSearchButton.IsEnabled = FundSearchTextBox.Text != "";
     }
 
-    private void AddFundButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private void AddFundButton_Click(object sender, RoutedEventArgs e)
     {
         MorningstarResponseLine result = FundSearchGridView.SelectedItem as MorningstarResponseLine;
         ViewModel.AddFund(result, averageCost);
-        AverageCostTextBox.IsEnabled = false;
         AverageCostTextBox.Text = "";
         FundSearchGridView.SelectedItem = null;
         AddFundButton.IsEnabled = false;
+        FundSearchGrid.Visibility = Visibility.Collapsed;
+        FundSearchGridView.Visibility = Visibility.Collapsed;
+        StepTwoGrid.Visibility=Visibility.Collapsed;
+        StepThreeGrid.Visibility = Visibility.Collapsed;
+        AddFundStackPannel.Visibility = Visibility.Collapsed;
     }
 
     private void FundSearchGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         bool fundSelected = FundSearchGridView.SelectedItem != null;
         AverageCostTextBox.IsEnabled = fundSelected;
-        AddFundStackPannel.Visibility = fundSelected ? Microsoft.UI.Xaml.Visibility.Visible : Microsoft.UI.Xaml.Visibility.Collapsed;
+        AddFundStackPannel.Visibility = fundSelected ? Visibility.Visible : Visibility.Collapsed;
+        StepThreeGrid.Visibility = fundSelected ? Visibility.Visible : Visibility.Collapsed;
     }
 
     private void AverageCostTextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -83,7 +90,7 @@ public sealed partial class PortfolioZoomPage : Page
         }
     }
 
-    private void BackButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private void BackButton_Click(object sender, RoutedEventArgs e)
     {
         if (this.Frame.CanGoBack)
             this.Frame.GoBack();
@@ -91,15 +98,19 @@ public sealed partial class PortfolioZoomPage : Page
             this.Frame.Navigate(typeof(PortfoliosPage));
     }
 
-    private void MenuFlyoutDeleteItem_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private void MenuFlyoutDeleteItem_Click(object sender, RoutedEventArgs e)
     {
         if (sender is MenuFlyoutItem item && item.DataContext is FundPerformance fund)
             ViewModel.RemoveFund(fund);
     }
 
-    private void SearchAccelerator_Invoked(Microsoft.UI.Xaml.Input.KeyboardAccelerator sender, Microsoft.UI.Xaml.Input.KeyboardAcceleratorInvokedEventArgs args)
+    private void SearchAccelerator_Invoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
     {
         if (FundSearchTextBox.Text != "")
             FundSearchButton_Click(sender, null);
+    }
+    private void AddFundAppBarButton_Click(object sender, RoutedEventArgs e)
+    {
+        FundSearchGrid.Visibility = Visibility.Visible;
     }
 }
