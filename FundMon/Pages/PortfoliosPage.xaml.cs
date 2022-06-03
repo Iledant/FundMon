b0ccea47511d0cdd0777f1ea3422e6f408369f18
Iledant <christophe.saintillan@gmail.com>
2022-06-03 23:15:20 +0200

    Refactor first page UI

--- FundMon/Pages/PortfoliosPage.xaml.cs
+++ FundMon/Pages/PortfoliosPage.xaml.cs
@@ -21,15 +21,18 @@ public sealed partial class PortfoliosPage : Page
         if (NameTextBox.Text == "")
             return;
         if (AddButton.Content as string == "Ajouter")
-        {
-            ViewModel.AddPortfolio(NameTextBox.Text, DescriptionTextBox.Text);
-            NameTextBox.Text = "";
-            DescriptionTextBox.Text = "";
-        }
+            AddPortfolio();
         else
-        {
             ViewModel.UpdatePortfolio(SelectedPortfolio.ID, NameTextBox.Text, DescriptionTextBox.Text);
-        }
+        AddEditGrid.Visibility = Visibility.Collapsed;
+    }
+
+    private void AddPortfolio()
+    {
+        ViewModel.AddPortfolio(NameTextBox.Text, DescriptionTextBox.Text);
+        NameTextBox.Text = "";
+        DescriptionTextBox.Text = "";
+
     }
 
     private void NameTextBox_TextChanged(object sender, Microsoft.UI.Xaml.Controls.TextChangedEventArgs e)
@@ -45,19 +48,17 @@ public sealed partial class PortfoliosPage : Page
 
     private void GridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        if (GridView.SelectedItem is Portfolio portfolio)
+        if (GridView.SelectedItem is Portfolio portfolio && portfolio != SelectedPortfolio)
         {
+            EditAppBarButton.IsEnabled = true;
             SelectedPortfolio = portfolio;
-            NameTextBox.Text = SelectedPortfolio.Name;
-            DescriptionTextBox.Text = SelectedPortfolio.Description;
-            AddButton.Content = "Modifier";
+            AddEditGrid.Visibility = Visibility.Collapsed;
         }
         else
         {
+            EditAppBarButton.IsEnabled = false;
+            AddEditGrid.Visibility = Visibility.Collapsed;
             SelectedPortfolio = null;
-            NameTextBox.Text = "";
-            DescriptionTextBox.Text = "";
-            AddButton.Content = "Ajouter";
         }
     }
 
@@ -76,5 +77,33 @@ public sealed partial class PortfoliosPage : Page
     {
         if (sender is MenuFlyoutItem item && item.DataContext is Portfolio portfolio)
             ViewModel.Portfolios.Remove(portfolio);
+    }
+
+    private void NameTextBoxAccelerator_Invoked(Microsoft.UI.Xaml.Input.KeyboardAccelerator sender, Microsoft.UI.Xaml.Input.KeyboardAcceleratorInvokedEventArgs args)
+    {
+        if (NameTextBox.Text != "")
+            AddPortfolio();
+    }
+
+    private void EditAppBarButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (GridView.SelectedItem is Portfolio portfolio)
+        {
+            SelectedPortfolio = portfolio;
+            NameTextBox.Text = SelectedPortfolio.Name;
+            DescriptionTextBox.Text = SelectedPortfolio.Description;
+            AddButton.Content = "Modifier";
+            AddEditGrid.Visibility = Visibility.Visible;
+        }
+    }
+
+    private void AddAppBarButton_Click(object sender, RoutedEventArgs e)
+    {
+        SelectedPortfolio = null;
+        NameTextBox.Text = "";
+        DescriptionTextBox.Text = "";
+        AddButton.Content = "Ajouter";
+        AddButton.IsEnabled = false;
+        AddEditGrid.Visibility = Visibility.Visible;
     }
 }
