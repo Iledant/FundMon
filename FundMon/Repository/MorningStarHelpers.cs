09886f0b37e842bb254378b8c4261bf26368ad81
Christophe SAINTILLAN <christophe.saintillan@gmail.com>
2022-06-15 21:25:03 +0200

    Add exception catching in morningstar fetch request

--- FundMon/Repository/MorningStarHelpers.cs
+++ FundMon/Repository/MorningStarHelpers.cs
@@ -45,34 +45,39 @@ internal static class MorningStarHelpers
     private readonly static NumberFormatInfo _numberFormat = new CultureInfo("en-US").NumberFormat;
     private readonly static string[] filteredCategories = { "PEA", "Fonds", "Actions" };
 
-public static async Task<List<DateValue>> GetHistoricalFromID(string MorningStarID, DateTime? beginDate = null, DateTime? endDate = null)
+    public static async Task<List<DateValue>> GetHistoricalFromID(string MorningStarID, DateTime? beginDate = null, DateTime? endDate = null)
     {
         string endPattern = (endDate ?? DateTime.Now).ToString("yyyy-MM-dd");
         string beginPattern = (beginDate ?? new DateTime(1991, 11, 29)).ToString("yyyy-MM-dd");
         string url = $"https://tools.morningstar.fr/api/rest.svc/timeseries_price/ok91jeenoo?" +
             $"id={MorningStarID}&currencyId=EUR&idtype=Morningstar&frequency=daily&" +
             $"startDate={beginPattern}&endDate={endPattern}&outputType=JSON";
-
-        HttpResponseMessage response = await _client.GetAsync(url);
-        string content = await response.Content.ReadAsStringAsync();
-        MorningStarPayloadRoot? root = JsonConvert.DeserializeObject<MorningStarPayloadRoot>(content);
-        List<HistoryDetail>? historyDetails = root?.TimeSeries?.Security?[0].HistoryDetail;
         List<DateValue> values = new();
 
-        if (historyDetails is null)
+        try
         {
-            return values;
-        }
+            HttpResponseMessage response = await _client.GetAsync(url);
+            string content = await response.Content.ReadAsStringAsync();
+            MorningStarPayloadRoot? root = JsonConvert.DeserializeObject<MorningStarPayloadRoot>(content);
+            List<HistoryDetail>? historyDetails = root?.TimeSeries?.Security?[0].HistoryDetail;
 
-        foreach (HistoryDetail h in historyDetails)
-        {
-            if (h.EndDate is null || h.EndDate.Length < 10 || h.Value is null)
+            if (historyDetails is null)
+                return values;
+
+            foreach (HistoryDetail h in historyDetails)
             {
-                throw new Exception("Erreur de format de réponse");
+                if (h.EndDate is null || h.EndDate.Length < 10 || h.Value is null)
+                {
+                    throw new Exception("Erreur de format de réponse");
+                }
+                values.Add(new DateValue(double.Parse(h.Value, _numberFormat), new DateTime(int.Parse(h.EndDate.Substring(0, 4)),
+                    int.Parse(h.EndDate.Substring(5, 2)),
+                    int.Parse(h.EndDate.Substring(8, 2)))));
             }
-            values.Add(new DateValue(double.Parse(h.Value, _numberFormat), new DateTime(int.Parse(h.EndDate.Substring(0, 4)),
-                int.Parse(h.EndDate.Substring(5, 2)),
-                int.Parse(h.EndDate.Substring(8, 2)))));
+        }
+        catch (Exception e)
+        {
+            Config.Config.AddLog($"Erreur de récupération des données depuis Morningstar : {e}");
         }
         return values;
     }
@@ -86,9 +91,8 @@ public static async Task<List<DateValue>> GetHistoricalFromID(string MorningStar
         {
             string[] fields = line.Split('|');
             if (fields.Length < 6)
-            {
                 continue;
-            }
+
             int left = fields[1].IndexOf("\"i\":\"") + 5;
             int right = fields[1].IndexOf("\",\"");
             if (left == -1 || right == -1 || right <= left)
@@ -118,7 +122,7 @@ public static async Task<List<DateValue>> GetHistoricalFromID(string MorningStar
     public static async Task<List<MorningstarResponseLine>> FetchFunds(string pattern)
     {
         string url = "https://www.morningstar.fr/fr/util/SecuritySearch.ashx?source=nav&moduleId=6&ifIncludeAds=False&usrtType=v";
-        FormUrlEncodedContent formContent = new (new[]
+        FormUrlEncodedContent formContent = new(new[]
         {
             new KeyValuePair<string, string>("q", pattern),
             new KeyValuePair<string, string>("limit", "100")
